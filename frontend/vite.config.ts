bace0f4933d1d8bb5a643e279e77b6d2092a9220
NIRUGUTTI MOKSHA SAI REDDY <62692898+mokshanirugutti@users.noreply.github.com>
2024-07-02 17:34:32 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,16 +4,7 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name].js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`
-      }
-    }
-  }
 })
