c3a03a8c10e98f75f46d4f34985c77ec38fb6e1e
mokshanirgutti <mokshasai910@gmail.com>
2024-07-02 17:01:56 +0530

    configuration for netlify

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,16 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
+  }
 })
